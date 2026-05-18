4e79e27793da9206052cbb90a0fa5ef46172f2a2
sarai <angelessaraiescatel@gmail.com>
2026-05-18 14:43:09 -0600

    Actualizaciones del proyecto

--- nodos/entrada/src/main.cpp
+++ nodos/entrada/src/main.cpp
@@ -1,18 +1,119 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// WIFI
+const char *ssid = "ESP32_TEST2";
+const char *password = "12345678";
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+//  IP de el servidor
+const char *serverName =
+    "http://172.20.10.2:8040/data";
+
+// Pines
+#define AVOID_PIN 18
+#define TRACK_PIN 19
+
+int personas = 0;
+
+// Estados anteriores
+int lastAvoid = 1;
+int lastTrack = 1;
+
+void setup()
+{
+
+  Serial.begin(115200);
+
+  pinMode(AVOID_PIN, INPUT);
+  pinMode(TRACK_PIN, INPUT);
+
+  // WiFi
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi conectado");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int avoid =
+      digitalRead(AVOID_PIN);
+
+  int tracking =
+      digitalRead(TRACK_PIN);
+
+  // =====================
+  // ENTRADA
+  // =====================
+
+  if (avoid == 0 && lastAvoid == 1)
+  {
+
+    personas++;
+
+    Serial.println("Persona ENTRO");
+  }
+
+  // =====================
+  // SALIDA
+  // =====================
+
+  if (tracking == 0 && lastTrack == 1)
+  {
+
+    personas--;
+
+    if (personas < 0)
+    {
+      personas = 0;
+    }
+
+    Serial.println("Persona SALIO");
+  }
+
+  // Guardar estados
+  lastAvoid = avoid;
+  lastTrack = tracking;
+
+  // JSON
+  String json = "{";
+
+  json += "\"id\":\"entrada\",";
+  json += "\"avoid\":" + String(avoid) + ",";
+  json += "\"tracking\":" + String(tracking) + ",";
+  json += "\"personas\":" + String(personas);
+
+  json += "}";
+
+  Serial.println(json);
+
+  // HTTP
+  if (WiFi.status() == WL_CONNECTED)
+  {
+
+    HTTPClient http;
+
+    http.begin(serverName);
+
+    http.addHeader(
+        "Content-Type",
+        "application/json");
+
+    int response =
+        http.POST(json);
+
+    Serial.print("HTTP: ");
+    Serial.println(response);
+
+    http.end();
+  }
+
+  delay(500);
 }
